a6fc21be748a13f3c40b5b97bd3b64b3b6e48554
Crummett <dominic.male@gmail.com>
2016-03-07 12:20:57 +0000

    Clean up and commenting

--- PhoneticSearcher.py
+++ PhoneticSearcher.py
@@ -1,77 +1,86 @@
 import sys
 
 def main():
-    
     inputNames, surnamesFile = getInputs()
+    #Read the surnames file, and make a list of the names.
     surnames = open(surnamesFile, 'r').read().splitlines()
+    equivalents = generateEquivalents()
     output = ""
+    # Generate a string of equivalents for each of the input names
     for name in inputNames:
-        output = output + match(name, surnames) + '\n'
+        output = output + match(name, surnames, equivalents) + '\n'
+    # Remove the trailing /n
     output = output[:-2]
+    
     print(output)
     
 def getInputs():
-    
-    inputNames = sys.argv[1:(len(sys.argv)-1)]
-    surnamesFile = sys.argv[(len(sys.argv)-1)]
+    # Return the input names. Ignoring all other input.
+    inputNames = sys.argv[1:-1]
+    # Takes the last 
+    surnamesFile = sys.argv[-1]
     
     return inputNames, surnamesFile
     
-def match(nameFromCMD, surnamesFileContents):
-    
+def match(nameFromCMD, surnamesFileContents, equivalentsDictionary):
     nameInput, surnamesList = nameFromCMD, surnamesFileContents
-    equivalents = generateEquivalents()
-    
-    name = disregarder(treat(nameInput), equivalents)
-    matches = []
+    equivalents, matches = equivalentsDictionary, []
+    # Cleans the string of any unnescessary letters
+    name = disregarder(cleanName(nameInput), equivalents)
+    # Cleans up each surname before comparing it to the input name
     for surname in surnamesList:
-        surnameToCheck = disregarder(treat(surname), equivalents)
+        surnameToCheck = disregarder(cleanName(surname), equivalents)
         if (compareEquivalents(name, surnameToCheck, equivalents)):
             matches.append(surname)
-    
+    # Creates the output string
     output = nameInput + ':'
     try:
         for match in matches:
             output = output + ' ' + match + ','
     except IndexError:
-        output = output + ' No matches found'
+        output = output + ' No matches found  '
+    if (matches == []):
+        output = output + ' No matches found  '
+    # Removes the trailing comma
     output = output[:-1]
     return output
 
 def compareEquivalents(nameInput, surnameInput, equivalentsDictionary):
     name, surname, equivalents = nameInput, surnameInput, equivalentsDictionary
-    finished, equivalent = False, True
-    
-    while (not finished):
-        for i in range(len(surname)):
-            try:
-                if (surname[i] not in equivalents[name[i]]):
-                    finished, equivalent = True, False
-            except IndexError:
-                finished, equivalent = True, False
-        finished = True
+    equivalent = True
+    # Initially assumes names are equivalent. Cycles through each letter of the
+    # surname comparing it to the letter at the same position of the input name
+    for i in range(len(surname)):
+        try:
+            if (surname[i] not in equivalents[name[i]]):
+                equivalent = False
+        except IndexError: # catches in case surname longer than input name
+            equivalent = False
     
     return equivalent
     
-def treat(nameInput):
-    
+def cleanName(nameInput):
+    # Converts the name to lower case
     name = nameInput.lower()
-    treatedName = ""
-    
+    cleanedName = ""
+    # Removes any non-chars
     for letter in name:
         if letter.islower():
-            treatedName = treatedName + letter
+            cleanedName = cleanedName + letter
     
-    return treatedName
+    return cleanedName
     
 def disregarder(nameInput, equivalentsDictionary):
     name, equivalents = nameInput, equivalentsDictionary
+    # Keeps the 1st letter
     firstStage = name[0]
+    # Removes any of the letters disregardable after the first
     for letter in name[1:]:
         if (letter not in ['a', 'e', 'i', 'h', 'o', 'u', 'w', 'y']):
             firstStage = firstStage + letter
-    
+    # Keeps the 1st letter
     secondStage = name[0]
+    # Removes any letters that are equivalent to the previous letter
     for letterNum in range(1, len(firstStage)):
         if (firstStage[letterNum] not in equivalents[firstStage[letterNum-1]]):
             secondStage = secondStage + firstStage[letterNum]
